191937cf2c16f7e0a3705aa3890617038ce2a411
lewqs <lewisbrown3108@gmail.com>
2023-01-17 10:47:45 +0000

    Added path not found handler, comments for the error handlers, test for path not found, completed task 6 and changed the error code displayed when review_id has correct data type, but is not found

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 const e = require("express");
 const express = require("express");
-const { getCategories, getReviews, getReviewById } = require("./controllers");
+const {
+  getCategories,
+  getReviews,
+  getReviewById,
+  getCommentsByReviewId,
+} = require("./controllers");
 const app = express();
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id", getReviewById);
+app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
 
+// Path Not Found Handler
+app.use((req, res, next) => {
+  res.status(404).send({ message: "Path Not Found" });
+});
+
+// Custom Error Handler
 app.use((err, request, response, next) => {
   if (err.status) response.status(err.status).send({ message: err.msg });
   else next(err);
 });
 
+// PostgreSQL Error Code Handler
 app.use((err, request, response, next) => {
   if (err.code === "22P02") {
     response.status(400).send({ message: "Bad Request" });
   } else next(err);
 });
 
+// Unhandled PostgreSQL Error Codes/Server Has Kaboomed
 app.use((err, request, response, next) => {
   console.log(err);
-  response.status(500).send("Internal server error");
+  response.status(500).send("Internal Server Error");
 });
 
 module.exports = app;
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,9 @@
-const { fetchCategories, fetchReviews, fetchReviewById } = require("../models");
+const {
+  fetchCategories,
+  fetchReviews,
+  fetchReviewById,
+  fetchCommentsByReviewId,
+} = require("../models");
 
 exports.getCategories = (req, res, next) => {
   fetchCategories().then((categories) => {
@@ -17,6 +22,15 @@ exports.getReviewById = (req, res, next) => {
   fetchReviewById(review_id)
     .then((review) => {
       res.status(200).send({ review });
+    })
+    .catch(next);
+};
+
+exports.getCommentsByReviewId = (req, res, next) => {
+  const { review_id } = req.params;
+  Promise.all([fetchCommentsByReviewId(review_id), fetchReviewById(review_id)])
+    .then(([comments]) => {
+      res.status(200).send({ comments });
     })
     .catch(next);
 };
