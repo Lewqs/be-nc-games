65ea90c74596ecb213b3f5cb7b4a3b50b2022a57
lewqs <lewisbrown3108@gmail.com>
2023-01-19 15:35:18 +0000

    Created a new test for comment_count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -163,7 +163,7 @@ describe("App", () => {
   });
 
   describe("GET /api/reviews/:review_id", () => {
-    test("200: Responds with a review object, which should have the properties: review_id, title, review_body, designer, review_img_url, votes, category, owner, created_at and comment_count", () => {
+    test("200: Responds with a review object, which should have the properties: review_id, title, review_body, designer, review_img_url, votes, category, owner and created_at", () => {
       return request(app)
         .get("/api/reviews/1")
         .expect(200)
@@ -178,6 +178,13 @@ describe("App", () => {
           expect(review).toHaveProperty("created_at", expect.any(String));
           expect(review).toHaveProperty("votes", expect.any(Number));
           expect(review).toHaveProperty("designer", expect.any(String));
+        });
+    });
+    test("200: Responds with a review object, which should have the property: comment_count", () => {
+      return request(app)
+        .get("/api/reviews/1")
+        .expect(200)
+        .then(({ body: { review } }) => {
           expect(review).toHaveProperty("comment_count", expect.any(Number));
         });
     });
